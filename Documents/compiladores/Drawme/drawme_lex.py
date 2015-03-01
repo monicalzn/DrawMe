bb9ee46440720039fa523fd5ff025d9dba2df56f
monicalzn <mnclzn@gmail.com>
2015-02-28 22:22:58 -0600

    adding the first official version of the parser (lex and yacc), done in PLY

---
+++ Documents/compiladores/Drawme/drawme_lex.py
@@ -0,0 +1,76 @@
+import ply.lex as lex
+
+reserved = {
+   'if' : 'IF',
+   'else' : 'ELSE',
+   'program' : 'PR',
+   'var' : 'V',
+   'main' : 'MAIN',
+   'int' : 'INT',
+   'float' : 'FLOAT',
+   'global' : 'GL',
+   'function': 'FUN',
+   'penwidth':'PENW',
+   'penpos':'PENP',
+   'pencolor':'PENC',
+   'penup': 'PENU',
+   'pendown':'PEND',
+   'setcolor':'SETC',
+   'backgroundcolor' : 'BACO',
+   'forward':'F',
+   'rectangle' :'REC',
+   'fill' :'FILL',
+   'triangle' :'TRI',
+   'circle' : 'CIR',
+   'square' : 'SQ',
+   'polygon' : 'POL',
+   'angle' : 'A',
+   'arc' : 'ARC',
+   'linestrip' : 'LS',
+   'back' : 'B',
+   'angle' : 'A',
+   'repeat' : 'RE',
+   'lista' : 'L',
+   'label' : 'LA'
+}
+
+#List of token name.
+tokens = [
+	'LB', 'RB', 'DP', 'C', 'LP', 'RP', 'EQ', 'VALI', 'VALF', 'SEQ', 'D', 'MT', 'LT', 'ADD', 'SUB', 'M', 'DIV', 'STR', 'SC', 'ID'
+	] + list(reserved.values())
+
+t_ignore 	= ' \t\n\r'
+t_EQ     	= r'='
+t_MT      	= r'>'
+t_LT      	= r'<'
+t_D             = r'<>'
+t_ADD     	= r'\+'
+t_SUB    	= r'-'
+t_M     	= r'\*'
+t_DIV  		= r'/'
+t_SC    	= r';'
+t_DP 		= r':'
+t_C     	= r'\,'
+t_LB    	= r'\{'
+t_RB    	= r'\}'
+t_LP    	= r'\('
+t_RP    	= r'\)'
+t_VALI  	= r'\d+'
+t_VALF  	= r'\d+\.\d+'
+t_STR   	= r'\'.*\''
+t_SEQ		= r'=='
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+# Error handling rule
+def t_error(t):
+        if(t.value[0] != None):
+	    print "Illegal character ", t.value[0] ,
+	    t.lexer.skip(1)
+
+lex.lex()
+
+
---
+++ Documents/compiladores/Drawme/drawme_yacc.py
@@ -0,0 +1,274 @@
+import ply.yacc as yacc
+import drawme_lex 
+import sys
+
+tokens = drawme_lex.tokens
+
+def p_prog(p):
+	'''prog : PR p2 p3 MAIN vars block'''
+
+def p_p2(p):
+	'''p2 : globals 
+| empty'''
+
+def p_p3(p):
+	'''p3 : functions 
+| empty'''
+
+def p_globals(p):
+	'''globals : GL vars'''
+
+def p_functions(p): 
+	'''functions : FUN ID fun2 DP vars block SC'''
+
+def p_fun2(p):
+        '''fun2 : LP fun3 RP'''
+
+def p_fun3(p):
+	'''fun3 : type ID fun4 '''
+
+def p_fun4(p):
+	'''fun4 : C type ID fun4  
+| empty'''
+
+def p_vars(p): 
+	'''vars : V var2 var5 SC 
+| empty'''
+
+def p_var2(p):
+        '''var2 : type var3  var22'''
+
+def p_var22(p):
+        '''var22 : C type var3 var22
+| empty'''
+
+def p_var3(p):
+	'''var3 : ID var4 var33 '''
+
+def p_var33(p):
+	'''var33 : C ID var4 var33 
+| empty '''
+
+def p_var4(p):
+        '''var4 : EQ val 
+| empty'''
+
+def p_var5(p):
+	'''var5 : list 
+| empty'''
+
+def p_type(p):
+	'''type : INT 
+			| FLOAT  '''
+
+def p_val(p):
+	'''val : VALI 
+			| VALF  '''
+
+def p_position(p):
+	'''position : PENP LP exp C exp RP SC'''
+
+def p_colors(p):
+	'''colors : PENC colors2 
+| SETC colors2 
+| BACO colors2 '''
+
+def p_colors2(p):
+	'''colors2 : LP exp C exp C exp RP SC'''
+
+def p_p_width(p):
+	'''p_width : PENW LP exp RP SC '''
+
+def p_penwrite(p):
+	'''penwrite : PENU SC 
+| PEND SC'''
+
+def p_move(p):
+	'''move : F mueve2
+| B mueve2'''
+
+def p_mueve2(p):
+	'''mueve2 : LP exp C A exp RP SC'''
+
+def p_rect(p):
+	'''rect : REC LP exp C exp p_fill RP SC'''
+
+def p_p_fill(p):
+	'''p_fill : C FILL 
+| empty'''
+
+def p_tria(p):
+	'''tria : TRI LP exp C exp C exp p_fill RP SC'''
+
+def p_one_par(p):
+	'''one_par : CIR opar2 
+| SQ opar2'''
+
+def p_opar2(p):
+	'''opar2 : LP exp p_fill RP SC'''
+
+def p_poly(p):
+	'''poly : POL LP list p_fill RP SC'''
+
+def p_lstrip(p):
+	'''lstrip : LS LP list RP SC'''
+
+def p_p_arc(p):
+	'''p_arc : ARC LP exp RP SC'''
+
+
+def p_expresion(p):
+	'''expresion : exp ex2 '''
+
+def p_ex2(p):
+	'''ex2 : ex3 exp 
+| empty'''
+
+def p_ex3(p):
+	'''ex3 : LT 
+| MT 
+| D 
+| SEQ'''
+
+def p_exp(p):
+	'''exp : term exp2'''
+#/* poper saca + o - */
+
+def p_exp2(p):
+	'''exp2 : exp3 exp 
+| empty'''
+
+def p_exp3(p):
+	'''exp3 : ADD 
+| SUB'''
+#/*mete a poper*/
+
+def p_term(p):
+	'''term : fact term2'''
+#/*une a poper * o / */
+
+def p_term2(p):
+	'''term2 : term3 term 
+| empty'''
+
+def p_term3(p):
+	'''term3 : M 
+| DIV'''
+#/*mete a poper*/
+
+def p_fact(p):
+	'''fact : LP exp RP 
+| fact2'''
+
+def p_fact2(p):
+	'''fact2 : fact3 fact4'''
+
+def p_fact3(p):
+	'''fact3 : ADD 
+| SUB 
+| empty'''
+
+def p_fact4(p):
+	'''fact4 : val 
+| ID'''
+
+def p_rep(p):
+	'''rep : RE exp block SC'''
+
+def p_WID(p):
+	'''WID : ID WID2'''
+
+def p_WID2(p):
+	'''WID2 : assigment
+| funCall'''
+
+def p_assigment(p):
+	'''assigment : EQ expresion SC'''
+
+def p_funCall(p):
+	'''funCall : LP func2 RP SC'''
+
+def p_func2(p):
+	'''func2 : exp func3 
+| empty'''
+
+def p_func3(p):
+	'''func3 : C exp 
+| empty'''
+
+def p_list(p):
+	'''list : L type ID EQ LP lista3 RP SC'''
+
+def p_lista2(p):
+	'''lista2 : val 
+| ID'''
+
+def p_lista3(p):
+	'''lista3 : lista2 li4'''
+
+def p_li4(p):
+	'''li4 : C lista2 li4
+| empty'''
+
+def p_lab(p):
+	'''lab : LA LP STR RP SC'''
+
+def p_condition(p):
+	'''condition : IF LP expresion RP block con2 SC'''
+
+def p_con2(p):
+	'''con2 : empty  
+| ELSE block'''
+
+def p_block(p):
+	'''block : LB block3 RB'''
+
+
+def p_block3(p):
+	'''block3 : block2 block3
+| empty '''
+
+def p_figure(p):
+	'''figure : rect
+| tria
+| poly
+| lstrip
+| one_par
+| p_arc'''
+
+def p_pen(p):
+	'''pen : colors 
+| p_width  
+| move 
+| position 
+| penwrite '''
+
+def p_block2(p):
+	'''block2 : figure 
+| condition 
+| pen 
+| lab 
+| list 
+| WID
+| rep'''
+
+def p_empty(p):
+    '''empty : '''
+
+# Error rule for syntax errors
+def p_error(p):
+    print "Syntax error in input!", p.type
+
+# Build the parser
+parser = yacc.yacc()
+
+if(len(sys.argv) > 1):
+    if sys.argv[1] == "-f":
+        f = open(sys.argv[2], "r")
+        s = f.readlines()
+    string = ""
+    for line in s:
+        string += line
+    result = parser.parse(string)
+else:
+    print "):"
