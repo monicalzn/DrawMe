af76a28294656cb991fa540c996abaab130520f2
monicalzn <mnclzn@gmail.com>
2015-05-06 02:22:27 -0500

    moooore comments

--- Documents/compiladores/Drawme/avail.py
+++ Documents/compiladores/Drawme/avail.py
@@ -145,6 +145,7 @@ class avail:
 	}
 
 	def get_temp(self, operator, type1, type2):
+		#depending on the result from the semantic cube a new temporal will be created
 		temp_type = self.get_type(operator, type1, type2)
 		if temp_type == 'int':
 			temp = self.temp_int
@@ -161,7 +162,8 @@ class avail:
 		temp += (self.block * 10000)
 		return [temp, self.get_type(operator, type1, type2)]
 
-	def get_type(self, operator, type1, type2): #operando1, operando2, operador
+	def get_type(self, operator, type1, type2): 
+#operando1, operando2, operador, returns the corresponding type from the operation
 		typ = self.semantic_cube.get(operator)
 		if typ != None:
 			typ = typ.get(type1)
@@ -171,6 +173,7 @@ class avail:
 		print 'error'	
 
 	def setblock(self, block):
+	#re-sets the memory for the next block
 		self.block = block
 		self.temp_int = 5000
 		self.temp_float = 6000
@@ -178,27 +181,33 @@ class avail:
 		self.temp_dir = 7000 
 
 	def getblock(self):
+	#returns the current block
 		return self.block
 	
 	def get_temp_dirs(self):
+	#returns the memory needed for the temporals
 		return [(self.temp_bool-4000), (self.temp_int-5000), (self.temp_float-6000), (self.temp_dir-7000)]
 
 	def expression(self):
+	#if there is an expression that needs to be resolved
 		if(self.OpStack.size() > 0):
 			if(self.OpStack.peek() == '>' or self.OpStack.peek() == '<' or self.OpStack.peek() == '<>' or self.OpStack.peek() == '==' or self.OpStack.peek() == '<=' or self.OpStack.peek() == '>='):
 				self.quad()
 
 	def add_sub(self):
+	#if there is an operation that needs to be solved.
 		if(self.OpStack.size() > 0):
 			if(self.OpStack.peek() == '+' or self.OpStack.peek() == '-'):
 				self.quad()
 
 	def mul_div(self):
+	#if there is an operation that needs to be solved.
 		if(self.OpStack.size() > 0):
 			if(self.OpStack.peek() == '*' or self.OpStack.peek() == '/'):
 				self.quad()
 
 	def rep_jump(self, dirOne, dirZero):
+	#it substracts one to the value of the temporal previously created with the value of the repetition, checks if it's equal to zero, if it's not it will do the block again, so a gotof is crated.
 		tem = self.OStack.pop()
 		spCuad = ['-', tem, dirOne, tem]
 		self.numQuad += 1
@@ -214,6 +223,7 @@ class avail:
 		self.quads.append(spCuad)
 
 	def assig(self, var):
+	#creates an assign
 		if(self.OStack.size() > 0):
 			self.numQuad += 1
 			spCuad = [101, self.OStack.pop(), -1, var]
@@ -221,6 +231,7 @@ class avail:
 			#meter temporal
 
 	def condition_start(self):
+	#at the end of the condition it fills the last goto that was created with the value of the current quad.
 		if(self.jumps.size() > 0):
 			jump = self.jumps.pop() - 1
 			spCuad = self.quads[jump]
@@ -228,6 +239,7 @@ class avail:
 			self.quads[jump] = spCuad
 
 	def condition(self):
+	#the start of the condition, creates a gotof and stores it, also stores the numQuad on the jump stack.
 		condition = self.OStack.pop()
 		cond_type = self.TStack.pop()
 		if(cond_type != "bool"):
@@ -240,6 +252,7 @@ class avail:
 			self.jumps.push(self.numQuad)
 
 	def condition_else(self):
+	#if there is an else the goto from the if must be filled and a new goto must be created.
 		jump = self.jumps.pop() -1
 		spCuad = self.quads[jump]
 		spCuad[3] = self.numQuad +1
@@ -250,18 +263,21 @@ class avail:
 		self.jumps.push(self.numQuad)
 	
 	def main(self):
+	#creates the goto of the main, stores the numQuad on the jump stack.
 		spCuad = ['GOTO', -1, -1, -1]
 		self.quads.append(spCuad)
 		self.jumps.push(self.numQuad)
 		self.numQuad += 1
 
 	def main_goto(self):
+	#fills the goto of the main.
 		jump = self.jumps.pop()
 		spCuad = self.quads[jump]
 		spCuad[3] = self.numQuad 
 		self.quads[jump] = spCuad
 		
 	def rep(self):
+	#makes a copy of the number that represents the amount of times the block will be made, and store the value to the stack.
 		h = self.get_temp('-', self.TStack.peek(), self.TStack.pop()) 
 		tem = h[0]
 		spCuad = [101, self.OStack.pop(), -1, tem]
@@ -272,6 +288,7 @@ class avail:
 		self.TStack.push(h[1])
 
 	def dim(self, dim, pointer):
+	#for arrays, creates the quad that will verify the value is within range, creates a pointer where the direction of the value that we want will be stored. stores the temopral.
 		spCuad = ['DIM', dim, pointer, -1]
 		self.numQuad += 1
 		self.quads.append(spCuad)
@@ -284,6 +301,7 @@ class avail:
 		self.OStack.push(tem)
 	
 	def dimT(self, dim, pointer):
+	#for matrix, pointer represents row, multiply by two to get the "real value", check is not out of bounds, get pointer dir, create quad that will generate actual direction, add one to the pointer to get the next value for which you need a new prointer dir and a quad that will generat actual direction.
 		h = self.get_temp('+', 'int', 'int') 
 		tem = h[0]
 		spCuad = ['*', pointer, '40002', tem]
@@ -337,6 +355,7 @@ class avail:
 		self.numQuad += 3
 
 	def dimP(self, direction, row, dim):
+	#check dimentio, within bounds, get pointer dir, quad that will generate actual direction.
 		spCuad = ['DIMC', dim, row, -1]
 		self.quads.append(spCuad)
 		
@@ -349,6 +368,7 @@ class avail:
 		self.numQuad += 2
 
 	def function_return(self, empty, vDir):
+	#check if function has return, if it does creates proper quad.
 		if(empty):
 			spCuad = ['RETURN',-1, -1, -1]
 			self.numQuad += 1
@@ -362,13 +382,14 @@ class avail:
 			return True
 
 	def function_end(self):
+	#end of procedure quad.
 		spCuad = ['ENDPROC', 1, -1, 1]
 		self.numQuad += 1
 		self.quads.append(spCuad)
 
 	def function_param(self, param):
+	#function parameters, check there are enough parameters, has to check the order of the parameters for correct assign.
 		cont = len(param)
-		print "PARPRPR", param, len(param) -1
 		if self.OStack.size() < cont:
 			print "Missing parameters."
 			sys.exit(0)
@@ -384,12 +405,14 @@ class avail:
 					self.quads.append(spCuad)
 
 	def call_function(self, var):
+	#quad that will store the name of the function for which memory is needed
 		spCuad = ['ERA', -1, -1, var]
 		self.numQuad += 1
 		self.quads.append(spCuad)
 		self.OpStack.push('(')
 
 	def call_function_end(self,var, vDir, temp):
+	#go sub that has the name of the function to which we will go, create an assign in case there is a returning value to a new temporal.
 		spCuad = ['GOSUB', -1, -1, var]
 		self.numQuad += 1
 		self.quads.append(spCuad)
@@ -400,42 +423,59 @@ class avail:
 		self.OpStack.pop()
 		
 	def append_quad(self, quad):
+	#appends quad
 		self.numQuad += 1
 		self.quads.append(quad)
 
 	def append_quad_one(self, fun):
+	#used by functions that only have one parameter.
 		self.numQuad += 1
 		spQuad = [fun, self.OStack.pop(), -1, -1]
 		self.TStack.pop()
 		self.quads.append(spQuad)
 
 	def append_quad_two(self, fun):
+	#used by functions that have two parameters.
 		self.numQuad += 1
 		two = self.OStack.pop()
+		self.TStack.pop()
 		spQuad = [fun, self.OStack.pop(),two, -1]
+		self.TStack.pop()
 		self.quads.append(spQuad)
 
 	def append_quad_three(self, fun):
+	#used by functions that have three parameters.
 		self.numQuad += 1
 		blue = self.OStack.pop()
+		self.TStack.pop()
 		green = self.OStack.pop()
+		self.TStack.pop()
 		spQuad = [fun, self.OStack.pop(), green, blue]
+		self.TStack.pop()
 		self.quads.append(spQuad)
 
 	def append_quad_tri(self, fun):
+	#used by triangle to create the necessary quads with the information in the right order.
 		self.numQuad += 2
 		y3 = self.OStack.pop()
+		self.TStack.pop()
 		x3 = self.OStack.pop()
+		self.TStack.pop()
 		y2 = self.OStack.pop()
+		self.TStack.pop()
 		x2 = self.OStack.pop()
+		self.TStack.pop()
 		y = self.OStack.pop()
+		self.TStack.pop()
 		x = self.OStack.pop()
+		self.TStack.pop()
 		spQuad = [fun, x, y, -1]
 		self.quads.append(spQuad)
 		spQuad = [x2, y2, x3, y3]
 		self.quads.append(spQuad)
 
 	def quad(self):
+	#used by expressions to create quads.
 		self.numQuad += 1
 		h = self.get_temp(self.OpStack.peek(), self.TStack.pop(), self.TStack.pop()) 
 		tem = h[0]
@@ -447,9 +487,11 @@ class avail:
 		self.TStack.push(h[1])
 
 	def printS(self):
+	#prints information.
 		print self.OStack.printi()
 
 	def get_temp_point(self):
+	#create a pointer direction
 		h = self.get_temp('$', 'dir', -1)
 		return h[0]
 		
